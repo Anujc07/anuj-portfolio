c86c5f3c1623124136a158a549c78319649436f9
Anujc07 <choubeyanuj44@gmail.com>
2024-09-30 17:48:03 +0530

    Initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>API Developer</h3>
+              <p>
+              Passionate API developer dedicated to building responsive, optimized solutions. I create seamless integrations that enhance user experiences and drive innovation, ensuring every project exceeds expectations. Let's connect and innovate together!
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+              Experienced in developing fast and optimized back-end systems using Django. I specialize in building robust APIs and scalable applications that enhance performance and deliver seamless user experiences.           
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Database Integration</h3>
+              <p>
+                I have designed multiple landing pages and have created design
+                systems as well
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img style={{ height: '30px' }}  src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:choubeyanuj44@gmail.com">choubeyanuj44@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img style={{ height: '30px' }}
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/anuj-choubey-9542a9249/">linkedin.com/anuj-choubey-9542a9249</a>
+        </li>
+        <li className={styles.link}>
+          <img style={{ height: '30px' }} src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Anujc07">github.com/Anujc07</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
